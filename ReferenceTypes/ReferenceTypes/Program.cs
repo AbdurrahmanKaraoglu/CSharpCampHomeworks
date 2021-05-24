fed8a30b773bc74f2618d3a16e8bd2d4944f5843
AbdurrahmanKaraoglu <18110131307@ogr.ksu.edu.tr>
2021-05-25 02:17:15 +0300

    Camp4Homework1

---
+++ ReferenceTypes/ReferenceTypes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ReferenceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int,decimal,float,double,enum,bool --> value types
+            int sayi1 = 10;
+            int sayi2 = 20;
+
+            sayi1 = sayi2;
+            sayi2 = 100;
+            Console.WriteLine("Sayı 1 : " + sayi1); // --> 20
+
+          
+        }
+    }
+}
